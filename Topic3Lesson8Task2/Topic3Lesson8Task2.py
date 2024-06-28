b4e7c04a1e5e3402499466990bb55976a0652626
Skroter <digro@yandex.ru>
2024-06-28 21:20:05 +0500

    Добавьте файлы проекта.

---
+++ Topic3Lesson8Task2/Topic3Lesson8Task2.py
@@ -0,0 +1,19 @@
+﻿N = int(input()) # создаем число случайное в диапазоне
+print(N, "- Число N")
+arr = list(map(int, input().split(None, N) [:N]))
+
+class C:
+    def rev(arr): 
+        return arr.reverse()
+
+C.rev(arr)
+print(arr)
+
+
+# import random
+
+# N = random.randint(1,100000) # создаем число случайное в диапазоне
+# print(N, "- Число N")
+# arr = [random.randint(1, 10000000000).split for i in range(N)]
+# arr.reverse() # переворачиваем массив
+# print(*arr)
---
+++ test/test.py
@@ -0,0 +1,33 @@
+﻿import random
+print('максимальная грузоподъемность лодки (кг):')
+m = random.randint(1,100) # получения случайного числа максимальной грузоподъемности 10е6(10 000 000)
+n = random.randint(1,10) # создаем число рыбаков случайное в диапазоне
+massa1n = [random.randint(1, m) for i in range(n)] # масса каждого рыбака
+massa1n.sort() # сортировка значений массива по возростанию
+count = 0 # колличество лодок
+
+x = 0
+
+while len(massa1n) > 1:
+        x = 0
+        summ = []
+        summ.append(massa1n[0])
+        summ.append(massa1n[-1])
+        x = sum(summ)
+        if x > m:
+            massa1n.pop()
+            print('один рыбак', massa1n)
+            count += 1
+        else:
+            massa1n.pop(0)
+            massa1n.pop()
+            print('два рыбака', massa1n)
+            count += 1
+            
+count += 1
+print(massa1n)
+
+
+
+
+print('Минимальное число лодок необходимое для переправы всех рыбаков: ', count)
